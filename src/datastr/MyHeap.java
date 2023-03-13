dd2575afe5bfb82fbf0b9a6b614943e27934e0a7
NiklavsBergs <bergs.niklavs@gmail.com>
2023-03-13 12:03:26 +0200

    Class myHeap

---
+++ src/datastr/MyHeap.java
@@ -0,0 +1,64 @@
+package datastr;
+
+public class MyHeap<T> {
+	private T elements[];
+	private final int DEFAULT_ARRAY_SIZE = 10;
+	private int arraySize = DEFAULT_ARRAY_SIZE;
+	private int elementCounter = 0;
+	
+	public MyHeap() {
+		elements = (T[]) new Object[arraySize];
+	}
+	
+	public MyHeap(int inputArraySize) {
+		
+		if(inputArraySize > 0) {
+			arraySize = inputArraySize;
+		}
+		elements = (T[]) new Object[arraySize];
+	}
+	
+	public boolean isFull(){
+		return elementCounter == arraySize;
+	}
+	
+	public boolean isEmpty(){
+		return elementCounter == 0;
+	}
+	
+	public int howManyElements() {
+		return elementCounter;
+	}
+	
+	private void increaseArray() {
+		int newSize;
+		T[] newElements;
+		if (elementCounter > 100) {
+			newSize = (int) (arraySize * 1.5);
+		}
+		else {
+			newSize = arraySize * 2;
+		}
+		newElements = (T[]) new Object[newSize];
+		
+		for (int i = 0; i < elementCounter; i++) {
+			newElements[i] = elements[i];
+		}
+		
+		elements = newElements;
+		arraySize = newSize;
+	}
+	
+	public void add(T newElement) {
+		if(isFull()) {
+			increaseArray();
+		}
+		
+		elements[elementCounter] = newElement;
+		elementCounter++;
+	}
+	
+	//public T getRightChild{
+		
+	//}
+}
